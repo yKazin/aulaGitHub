b982fa57211a7df7687b6b326d048661756b60b9
Morgado <Morgado@HOME_PC>
2022-07-30 10:28:32 -0300

    Começamos a criação do preojeto

---
+++ primeiro-projeto/src/cursoJava/execultavel/PrimeiraClass.java
@@ -0,0 +1,9 @@
+package cursoJava.execultavel;
+
+public class PrimeiraClass {
+	// Mainsn é um metodo execultavel
+	public static void main(String[] args) {
+		// escreve na tela 
+		System.err.println("Ola Mundo EM JAVA");
+	}
+}
